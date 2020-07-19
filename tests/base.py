1289806cdb98fc6f002c30da28b32e75ac387b19
whoami <admin@imipy.com>
2020-07-19 11:17:05 +0800

    tests

--- tests/base.py
+++ tests/base.py
@@ -7,6 +7,7 @@ from flask_mico.app import create_app
 os.environ.setdefault('FLASKMICO_SETTINGS_MODULE', 'conf.development.settings')
 application = create_app()
 
+
 @pytest.fixture
 def client():
     with application.test_client() as client:
